1b5fe8c23f2c3390e849dfd71d012a18747bb0a2
Vedant Helwatkar <vedanthelwatkar@rathi.com>
2025-04-08 12:06:33 +0530

    bg-neutral-50 added to top div

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -48,33 +48,34 @@ export function TimelineItem({
       </div>
 
       <div className="flex-1 sm:pb-12 pb-6 w-full px-5 lg:px-0 md:px-0">
-        <div className="w-full rounded-lg flex flex-col items-center md:items-start bg-neutral-50 h-fit">
-          <div className="relative z-30 w-full flex justify-between bg-brand-primary px-6 py-2 text-neutral-0 rounded-t-lg">
-            <span className="font-semibold text-body-sm text-neutral-0">
-              LEARNSHACK
-            </span>
-            <span className="font-semibold text-body-sm text-neutral-0">
-              {step}
-            </span>
-          </div>
-
-          <div className="relative z-30 sm:pt-9 sm:px-7 sm:pb-6 p-5 pb-8 bg-neutral-0 rounded-b-lg">
-            <div className="flex flex-col sm:flex-row gap-8 sm:gap-10">
-              <div className="flex flex-col gap-3 sm:gap-4">
-                <h3 className="text-h6 font-heading font-semibold text-neutral-800">
-                  {title}
-                </h3>
-                <p className="text-neutral-900 text-body-lg">{description}</p>
-              </div>
-              <div className="flex-shrink-0 flex items-center justify-center">
-                {image}
-              </div>
+        <div className="w-full rounded-lg flex flex-col items-center md:items-start h-fit relative">
+          <div className="relative z-30 bg-neutral-50">
+            <div className="w-full flex justify-between bg-brand-primary px-6 py-2 text-neutral-0 rounded-t-lg">
+              <span className="font-semibold text-body-sm text-neutral-0">
+                LEARNSHACK
+              </span>
+              <span className="font-semibold text-body-sm text-neutral-0">
+                {step}
+              </span>
             </div>
-            <span className="inline-flex items-center justify-center p-3 text-body-xl text-neutral-600 bg-neutral-0 rounded-full absolute left-1/2 -translate-x-1/2 -bottom-6 sm:left-7 sm:translate-x-0 border border-neutral-200 w-12 h-12">
-              VS
-            </span>
-          </div>
 
+            <div className="sm:pt-9 sm:px-7 sm:pb-6 p-5 pb-8 bg-neutral-0 rounded-b-lg">
+              <div className="flex flex-col sm:flex-row gap-8 sm:gap-10">
+                <div className="flex flex-col gap-3 sm:gap-4">
+                  <h3 className="text-h6 font-heading font-semibold text-neutral-800">
+                    {title}
+                  </h3>
+                  <p className="text-neutral-900 text-body-lg">{description}</p>
+                </div>
+                <div className="flex-shrink-0 flex items-center justify-center">
+                  {image}
+                </div>
+              </div>
+              <span className="inline-flex items-center justify-center p-3 text-body-xl text-neutral-600 bg-neutral-0 rounded-full absolute left-1/2 -translate-x-1/2 -bottom-6 sm:left-7 sm:translate-x-0 border border-neutral-200 w-12 h-12">
+                VS
+              </span>
+            </div>
+          </div>
           <div className="sticky bottom-6 z-10 pt-8 pb-6 px-7 flex flex-col gap-3 bg-neutral-50 rounded-b-lg ">
             <h4 className="font-medium text-neutral-600 text-body-xl">
               Traditional consultancies
