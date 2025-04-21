d774597c4f67b7360e91ffc9889318b003cc5f23
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-21 13:25:45 +0530

    scrollbar hidden for movile and overflow x hidden

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -53,7 +53,7 @@ const App = () => {
   if (isMobile) {
     return (
       <LayoutProvider>
-        <div className="flex flex-col min-h-dvh w-full">
+        <div className="flex flex-col min-h-dvh overflow-x-hidden w-full">
           <BrowserRouter>
             <ScrollToTop>
               <NavBar />
@@ -112,7 +112,9 @@ const App = () => {
   // For desktop, keep the original approach
   return (
     <LayoutProvider>
-      <div className="flex flex-col h-dvh w-full">
+      <div
+        className={`flex flex-col h-dvh overflow-x-hidden w-full ${"scrollbar-hide"}`}
+      >
         <BrowserRouter>
           <ScrollToTop>
             <NavBar />
