c950238751539f39cc54e19111af12df66964460
Vedant Elwatkar <vedanthelwatkar@gmail.com>
2025-04-22 16:08:53 +0530

    added step 2

--- src/components/contact-page/ContactFormContainer.jsx
+++ src/components/contact-page/ContactFormContainer.jsx
@@ -48,7 +48,7 @@ const ContactFormContainer = () => {
 
   useEffect(() => {
     if (isOtpSent) {
-      setCurrentStep(3);
+      setCurrentStep(2);
     }
   }, [isOtpSent]);
 
@@ -66,8 +66,7 @@ const ContactFormContainer = () => {
       phoneNumber: data.phoneNumber,
       countryCode: data.countryCode,
     });
-    // sendOtp(data.phoneNumber, data.countryCode);
-    setState({ isOtpSent: true });
+    sendOtp(data.phoneNumber, data.countryCode);
   };
 
   const handleStep2Submit = async (otp) => {
@@ -125,20 +124,20 @@ const ContactFormContainer = () => {
         />
       ),
     },
-    // {
-    //   id: 2,
-    //   component: (
-    //     <ContactStep2
-    //       phoneNumber={`${formData.countryCode} ${formData.phoneNumber}`}
-    //       onVerify={handleStep2Submit}
-    //       onResend={handleResendOTP}
-    //       setCurrentStep={setCurrentStep}
-    //       isLoading={isLoading}
-    //       error={error}
-    //       resendCooldown={resendCooldown}
-    //     />
-    //   ),
-    // },
+    {
+      id: 2,
+      component: (
+        <ContactStep2
+          phoneNumber={`${formData.countryCode} ${formData.phoneNumber}`}
+          onVerify={handleStep2Submit}
+          onResend={handleResendOTP}
+          setCurrentStep={setCurrentStep}
+          isLoading={isLoading}
+          error={error}
+          resendCooldown={resendCooldown}
+        />
+      ),
+    },
     {
       id: 3,
       component: <ContactStep3 onSubmit={handleFinalSubmit} />,
